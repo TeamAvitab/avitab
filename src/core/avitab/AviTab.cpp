TeamAvitab/avitab

commit 8cba75f54b0fdfed17eabaf8fa2d275cc93de09c

fix: don't overwrite badly formatted config file with default
This bug was introduced during the maintenance handover, but not
detected (not tested!) It doesn't merit a patch release.

src/core/env/Config.cpp
@@ -25,11 +25,13 @@ using json = nlohmann::json;
 
 namespace avitab {
 
+class missing_file : public std::exception { };
+
 inline static std::shared_ptr<nlohmann::json> ReadConfig(const std::string& configFile) {
-    // will raise an exception if file does not exist, or error when reading
+    // will raise an exception if file does not exist, or if there is a json format error when reading
     std::ifstream configStream(std::filesystem::u8path(configFile));
     if (!configStream) {
-        throw std::runtime_error(std::string("Couldn't read config file ") + configFile);
+        throw missing_file();
     }
     auto cfg = std::make_shared<json>();
     configStream >> *cfg;
@@ -37,18 +39,18 @@ inline static std::shared_ptr<nlohmann::json> ReadConfig(const std::string& conf
 }
 
 Config::Config(const std::string& configFile, const std::string &createDefault) {
+    assert(createDefault.size());
     try {
+        // First attempt to read a json config from the file.
         config = ReadConfig(configFile);
         return;
-    } catch (const std::exception &e) {
-        if (createDefault.size()) {
-            // Probably this is the first time that Avitab has been run after a clean install.
-            // The installation packages no longer include files that might be updated by
-            // the user, because we don't want subsequent installations to overwrite these.
-            // Avitab will attempt to create the default file if missing and try again.
-            std::ofstream configStream(std::filesystem::u8path(configFile));
-            configStream << createDefault;
-        }
+    } catch (const missing_file &e) {
+        // Probably this is the first time that Avitab has been run after a clean install.
+        // The installation packages no longer include files that might be updated by
+        // the user, because we don't want subsequent installations to overwrite these.
+        // Avitab will attempt to create the default file if missing and try again.
+        std::ofstream configStream(std::filesystem::u8path(configFile));
+        configStream << createDefault;
     }
 
     // Second attempt. If it fails just propagate the exception, no recovery possible.

commit ebcb97de7a90c8fcb37225c75ff0554f2362ce40

fix: bogus assert introduced in previous commit 8cba75f

src/core/avitab/AviTab.cpp
@@ -238,7 +238,7 @@ void AviTab::finishInstall() {
 void AviTab::createPanel() {
     auto cfgFile = getAirplanePath() + "/AviTab.json";
     try {
-        Config cfg(cfgFile, "");
+        Config cfg(cfgFile);
         int left = cfg.getInt("/panel/left");
         int bottom = cfg.getInt("/panel/bottom");
         int width = cfg.getInt("/panel/width");

src/core/env/Config.cpp
@@ -28,7 +28,7 @@ namespace avitab {
 class missing_file : public std::exception { };
 
 inline static std::shared_ptr<nlohmann::json> ReadConfig(const std::string& configFile) {
-    // will raise an exception if file does not exist, or if there is a json format error when reading
+    // Raises an exception if file does not exist, or if there is a json format error when reading
     std::ifstream configStream(std::filesystem::u8path(configFile));
     if (!configStream) {
         throw missing_file();
@@ -38,22 +38,26 @@ inline static std::shared_ptr<nlohmann::json> ReadConfig(const std::string& conf
     return cfg;
 }
 
+Config::Config(const std::string& configFile) {
+    // No recovery from any exceptions raised, just let them propogate.
+    config = ReadConfig(configFile);
+}
+
 Config::Config(const std::string& configFile, const std::string &createDefault) {
-    assert(createDefault.size());
     try {
         // First attempt to read a json config from the file.
         config = ReadConfig(configFile);
-        return;
+        return; // no exceptions - all done.
     } catch (const missing_file &e) {
         // Probably this is the first time that Avitab has been run after a clean install.
         // The installation packages no longer include files that might be updated by
         // the user, because we don't want subsequent installations to overwrite these.
-        // Avitab will attempt to create the default file if missing and try again.
+        // Attempt to create the default file if missing and try again.
         std::ofstream configStream(std::filesystem::u8path(configFile));
         configStream << createDefault;
     }
 
-    // Second attempt. If it fails just propagate the exception, no recovery possible.
+    // Second attempt. If it fails just propagate the exception, no further recovery possible.
     config = ReadConfig(configFile);
 }
 

src/core/env/Config.h
@@ -25,6 +25,7 @@ namespace avitab {
 
 class Config {
 public:
+    Config(const std::string &configFile);
     Config(const std::string &configFile, const std::string &createDefault);
 
     std::string getString(const std::string &pointer);

src/core/env/Environment.cpp
@@ -45,7 +45,8 @@ void Environment::loadNavWorldInBackground() {
 }
 
 void Environment::loadConfig() {
-    config = std::make_unique<Config>(getProgramPath() + "/config.json", R"({ "AviTab": { "logToStdOut": false, "loadNavData": true } })");
+    config = std::make_unique<Config>(getProgramPath() + "/config.json",
+                            R"({ "AviTab": { "logToStdOut": false, "loadNavData": true } })");
 }
 
 std::shared_ptr<Config> Environment::getConfig() {