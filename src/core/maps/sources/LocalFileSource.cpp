TeamAvitab/avitab

commit 61ed2e0bcad09b4ed48cc1d5437b93650bbec581

refactor: convert to use std::filesystem library

src/core/maps/sources/LocalFileSource.cpp
@@ -24,7 +24,7 @@
 
 namespace maps {
 
-LocalFileSource::LocalFileSource(const std::string& file, std::shared_ptr<apis::ChartService> chartService)
+LocalFileSource::LocalFileSource(const std::filesystem::path& file, std::shared_ptr<apis::ChartService> chartService)
 :   DocumentSource(file),
     utf8FileName(file),
     chartService(chartService)
@@ -35,7 +35,7 @@ LocalFileSource::LocalFileSource(const std::string& file, std::shared_ptr<apis::
         logger::info("No calibration: %s", e.what());
     }
 }
-LocalFileSource::LocalFileSource(const std::string& file, std::string calibrationMetadata)
+LocalFileSource::LocalFileSource(const std::filesystem::path& file, std::string calibrationMetadata)
 :   DocumentSource(file),
     utf8FileName(file)
 {
@@ -82,8 +82,8 @@ void LocalFileSource::attachCalibration3Angle(double angle)
 
 void LocalFileSource::findAndLoadCalibration() {
     // Try a co-located name-matched json file for calibration
-    std::string calFileName = utf8FileName + ".json";
-    std::ifstream jsonFile(std::filesystem::u8path(calFileName));
+    auto calFileName = utf8FileName /".json";
+    std::ifstream jsonFile(calFileName);
 
     if (jsonFile.good()) {
         logger::info("Loaded co-located json calibration file for %s", utf8FileName.c_str());
@@ -92,8 +92,8 @@ void LocalFileSource::findAndLoadCalibration() {
         calibration.fromJsonString(jsonStr, rasterizer.getAspectRatio(0));
     } else {
         // Try a co-located name-matched Google Earth KML file for calibration
-        std::string kmlFileName = utf8FileName + ".kml";
-        std::ifstream kmlFile(std::filesystem::u8path(kmlFileName));
+        auto kmlFileName = utf8FileName /".kml";
+        std::ifstream kmlFile(kmlFileName);
         if (kmlFile.good()) {
             logger::info("Loaded co-located kml calibration file for %s", utf8FileName.c_str());
             std::string kmlStr((std::istreambuf_iterator<char>(kmlFile)),
@@ -128,8 +128,8 @@ void LocalFileSource::storeCalibration() {
     }
     try {
         calibration.setHash(crypto.getFileSha256(utf8FileName));
-        std::string calFileName = utf8FileName + ".json";
-        std::ofstream jsonFile(std::filesystem::u8path(calFileName));
+        auto calFileName = utf8FileName /".json";
+        std::ofstream jsonFile(calFileName);
         jsonFile << calibration.toString();
     } catch (const std::exception &e) {
         logger::warn("Couldn't store calibration: %s", e.what());

src/core/maps/sources/LocalFileSource.h
@@ -24,8 +24,8 @@ namespace maps {
 
 class LocalFileSource: public DocumentSource {
 public:
-    LocalFileSource(const std::string& file, std::string calibrationMetadata);
-    LocalFileSource(const std::string& file, std::shared_ptr<apis::ChartService> chartService);
+    LocalFileSource(const std::filesystem::path& file, std::string calibrationMetadata);
+    LocalFileSource(const std::filesystem::path& file, std::shared_ptr<apis::ChartService> chartService);
 
     void attachCalibration1(double x, double y, double lat, double lon, int zoom) override;
     void attachCalibration2(double x, double y, double lat, double lon, int zoom) override;
@@ -37,7 +37,7 @@ private:
     void storeCalibration();
 
 private:
-    std::string utf8FileName;
+    std::filesystem::path utf8FileName;
     std::shared_ptr<apis::ChartService> chartService;
     apis::Crypto crypto;
 

commit b44db35d61f92f18724247fa1484ab722fbd4c59

bugfix: broken by 61ed2e0: incorrect manufacturing of calibration metafile names

src/core/maps/sources/LocalFileSource.cpp
@@ -82,7 +82,7 @@ void LocalFileSource::attachCalibration3Angle(double angle)
 
 void LocalFileSource::findAndLoadCalibration() {
     // Try a co-located name-matched json file for calibration
-    auto calFileName = utf8FileName /".json";
+    auto calFileName = makeCalibrationMetafileName("json");
     std::ifstream jsonFile(calFileName);
 
     if (jsonFile.good()) {
@@ -92,7 +92,7 @@ void LocalFileSource::findAndLoadCalibration() {
         calibration.fromJsonString(jsonStr, rasterizer.getAspectRatio(0));
     } else {
         // Try a co-located name-matched Google Earth KML file for calibration
-        auto kmlFileName = utf8FileName /".kml";
+        auto kmlFileName = makeCalibrationMetafileName("kml");
         std::ifstream kmlFile(kmlFileName);
         if (kmlFile.good()) {
             logger::info("Loaded co-located kml calibration file for %s", utf8FileName.c_str());
@@ -128,12 +128,19 @@ void LocalFileSource::storeCalibration() {
     }
     try {
         calibration.setHash(crypto.getFileSha256(utf8FileName));
-        auto calFileName = utf8FileName /".json";
+        auto calFileName = makeCalibrationMetafileName("json");
         std::ofstream jsonFile(calFileName);
         jsonFile << calibration.toString();
     } catch (const std::exception &e) {
         logger::warn("Couldn't store calibration: %s", e.what());
     }
+}
+
+std::filesystem::path LocalFileSource::makeCalibrationMetafileName(const std::string &ext) {
+    auto d = utf8FileName;
+    d.remove_filename();
+    auto f = utf8FileName.filename().u8string() + "." + ext;
+    return d / std::filesystem::u8path(f);
 }
 
 }

src/core/maps/sources/LocalFileSource.h
@@ -18,6 +18,7 @@
 #pragma once
 
 #include "DocumentSource.h"
+#include <filesystem>
 #include <string>
 
 namespace maps {
@@ -35,6 +36,7 @@ public:
 private:
     void findAndLoadCalibration();
     void storeCalibration();
+    std::filesystem::path makeCalibrationMetafileName(const std::string &ext);
 
 private:
     std::filesystem::path utf8FileName;